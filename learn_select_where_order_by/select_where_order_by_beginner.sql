06f0cdf275149906bdfebff84e99095aeb94961d
Francisco Gomes <fgsj.dev@outlook.com>
2025-09-29 22:10:31 -0300

    first commit

---
+++ learn_select_where_order_by/select_where_order_by_beginner.sql
@@ -0,0 +1,12 @@
+/*
+Author: Francisco Gomes da Silva Junior
+College: Estacio de Sa
+Course: Computer Science
+Date release: 29-09-2025, in format Brazil
+Version: 1.0
+Description: File with scripts of exercises "SELECT", "WHERE", "ORDER BY"
+*/
+
+--exercise 01: return all clients of table SalesLT.Customer
+SELECT *
+FROM SalesLT.Customer as sc
