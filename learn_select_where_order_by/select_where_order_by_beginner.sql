c08bd41bd6973f9caeb55e5a88f51f04787b9373
Francisco Gomes <fgsj.dev@outlook.com>
2025-09-29 22:13:18 -0300

    update file - exercise 02

--- learn_select_where_order_by/select_where_order_by_beginner.sql
+++ learn_select_where_order_by/select_where_order_by_beginner.sql
@@ -7,6 +7,11 @@ Version: 1.0
 Description: File with scripts of exercises "SELECT", "WHERE", "ORDER BY"
 */
 
---exercise 01: return all clients of table SalesLT.Customer
+-- exercise 01: return all clients of table SalesLT.Customer
 SELECT *
 FROM SalesLT.Customer as sc
+
+-- exercise 02: return first name and middle name of customers in order ascendent 
+SELECT sc.FirstName as 'Primeiro nome', sc.LastName as 'Ultimo nome'
+FROM SalesLT.Customer as sc
+ORDER BY sc.FirstName ASC
