73675e40aa7604412cb4f5f0ecd05dcc8c36891a
Francisco Gomes <fgsj.dev@outlook.com>
2025-09-30 21:57:07 -0300

    update file - exercise 01

---
+++ learn_sum_avg_count_min_max/sum_avg_count_min_max_beginner.sql
@@ -0,0 +1,13 @@
+/*
+Author: Francisco Gomes da Silva Junior
+College: Estacio de Sa
+Course: Computer Science
+Date release: 29-09-2025, in format Brazil
+Version: 1.0
+Description: File with scripts of exercises "SUM, AVG, COUNT, MIN, MAX"
+*/
+
+-- exercise 01: count amount clients in table
+SELECT COUNT(*) as 'Qtd de clientes na tabela'
+FROM SalesLT.Customer as sc
+
