c57d2b74b4e3b4ba443b6a2171d16900fe353d0d
Francisco Gomes <fgsj.dev@outlook.com>
2025-10-03 10:38:13 -0300

    update file - header of data file

---
+++ learn_joins/joins_beginner.sql
@@ -0,0 +1,9 @@
+/*
+Author: Francisco Gomes da Silva Junior
+College: Estacio de Sa
+Course: Computer Science
+Date release: 03-10-2025, in format Brazil
+Version: 1.0
+Description: File with scripts of exercises JOINS "INNER, LEFT, RIGHT, FULL"
+*/
+
